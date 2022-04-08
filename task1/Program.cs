c8cf80d78ffb4b32ab9225802b3feedfbb39154c
Dasha <steshitc@mail.ru>
2022-04-08 16:57:24 +0300

    add file with programm

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,9 @@
 ﻿//1) Показать двумерный массив размером m×n заполненный вещественными числами
+using System;
+
 void fillArray(double[,] arr)
 {
-	var rnd = new Random();
+	Random rnd = new Random();
 	for(int i = 0; i < arr.GetLength(0); i++)
 	{
 		for(int j = 0; j < arr.GetLength(1); j++)
@@ -15,7 +17,7 @@ void printArray(double[,] arr)
 	for(int i = 0; i < arr.GetLength(0); i++)
 	{
 		for(int j = 0; j < arr.GetLength(1); j++)
-			Console.Write(String.Format("{0, 4}", arr[i,j]) );
+			Console.Write(String.Format("{0, 4} ", arr[i,j]) );
 		Console.WriteLine();	
 	}	
 }	
